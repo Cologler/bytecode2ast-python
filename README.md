8caa22ac3304d93a866552d745ec5be44ff930c7
Cologler <skyoflw@gmail.com>
2019-05-11 00:37:16 +0800

    docs

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 
 A library for turn python byetcode into abstract syntax tree (`ast`).
 
+## Usage
+
+``` py
+from bytecode2ast import parse_func
+
+func_def_ast: ast.FunctionDef = parse_func(some_func)
+```
+
 ## Testing
 
 The decompiled ast node should have the same instructions with original python object.
+
+## Other Libs
+
+### uncompyle6
+
+``` py
+import ast
+from uncompyle6 import deparse_code2str
+
+src_code = deparse_code2str(some_func.__code__, out=io.StringIO())
+module_ast: ast.Module = ast.parse(src_code) # <- but I need FunctionDef!
+```
--- bytecode2ast/__init__.py
+++ bytecode2ast/__init__.py
@@ -5,14 +5,33 @@
 #
 # ----------
 
+from typing import Union, List
+import ast
+
 from .parsers.func_def import from_func
 
 def parse_func(func):
-    ''' return a `ast.FunctionDef` object from a function '''
+    '''
+    return a `ast.FunctionDef` object or `ast.Lambda` object from a function
+    '''
     return from_func(func)
 
-def create_module(ast_objs: list):
-    import ast
-    return ast.Module(
-        body=list(ast_objs)
+def create_module(ast_objs: Union[List[ast.AST], ast.AST]):
+    '''
+    create a `ast.Module` with body `ast_objs`.
+
+    This is helpful like `create_module(parse_func(?))`
+    '''
+
+    mod = ast.Module(
+        body=[]
     )
+
+    if isinstance(ast_objs, list):
+        mod.body.extend(ast_objs)
+    elif isinstance(ast_objs, ast.AST):
+        mod.body.append(ast_objs)
+    else:
+        raise TypeError
+
+    return mod
